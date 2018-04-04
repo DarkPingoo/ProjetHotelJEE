8f75afe5327b3bda3a766561a4fc8f921c8a1c4a
Pingoo <tristanlegacque@orange.fr>
2018-04-04 14:26:56 +0200

    Recreation SQL

--- sql/Insertion.sql
+++ sql/Insertion.sql
@@ -17,10 +17,17 @@ INSERT INTO CHAMBRE(typeChambre,nombrePlaceLit,PrixJournalier,etage) VALUES('Tau
 INSERT INTO CHAMBRE(typeChambre,nombrePlaceLit,PrixJournalier,etage) VALUES('Suite Royale',5,150,3);
 INSERT INTO CHAMBRE(typeChambre,nombrePlaceLit,PrixJournalier,etage) VALUES('Affaire',2,80,3);
 INSERT INTO CHAMBRE(typeChambre,nombrePlaceLit,PrixJournalier,etage) VALUES('Familiale',3,79,1);
+INSERT INTO CHAMBRE(typeChambre,nombrePlaceLit,PrixJournalier,etage) VALUES('Taudis',2,25,4);
 
 
 /* REMPLISSAGE DE LA TABLE RESERVATION */
-INSERT INTO RESERVATION(idChambre,idClient,dateDebut,dateFin,nombrePlaces,booleenPaiementEffectue) VALUES (1,2,'2018-11-02','2018-11-09',2,0);
-INSERT INTO RESERVATION(idChambre,idClient,dateDebut,dateFin,nombrePlaces,booleenPaiementEffectue) VALUES (2,1,'2018-11-02','2018-11-09',5,1);
-INSERT INTO RESERVATION(idChambre,idClient,dateDebut,dateFin,nombrePlaces,booleenPaiementEffectue) VALUES (3,4,'2018-11-02','2018-11-05',1,1);
-INSERT INTO RESERVATION(idChambre,idClient,dateDebut,dateFin,nombrePlaces,booleenPaiementEffectue) VALUES (4,3,'2018-11-03','2018-11-09',3,0);
+
+INSERT INTO `RESERVATION` (`idReservation`, `idChambre`, `idClient`, `dateDebut`, `dateFin`, `nombrePlaces`, `booleenPaiementEffectue`) VALUES
+  (1, 1, 2, '2018-11-02', '2018-11-09', 2, 1),
+  (2, 2, 1, '2018-11-02', '2018-11-09', 5, 1),
+  (3, 3, 4, '2018-11-02', '2018-11-05', 1, 1),
+  (4, 4, 3, '2018-11-03', '2018-11-09', 3, 1),
+  (5, 4, 3, '2018-05-05', '2018-05-09', 4, 1),
+  (13, 4, 2, '2018-10-16', '2018-10-19', 5, 1),
+  (15, 2, 1, '2018-03-23', '2018-03-28', 1, 0),
+  (16, 1, 2, '2018-01-03', '2018-01-07', 2, 0);
